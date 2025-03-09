874d4e10f09e0e297ae87c071340b4ec1bb1811b
Ubuntu <ubuntu@ip-172-31-10-211.us-east-2.compute.internal>
2025-03-09 05:36:58 +0000

    Correction 2

--- list-users2.sh
+++ list-users2.sh
@@ -8,36 +8,38 @@
 #################################
 
 #GitHub API Base URL
-API_URL = "https://api.github.com"
+API_URL="https://api.github.com"
 
 
 #GitHub Username and Personal Access
-USERNAME = $username
-TOKEN = $token
+USERNAME=$username
+TOKEN=$token
 
 #Owner and Repository information
-REPO_OWNER = $1
-REPO_NAME = $2
+REPO_OWNER=$1
+REPO_NAME=$2
 
 #Function to make get request to the GitHub API
-function github_get_api{
-	local endpoint = "$1"
-	local url = "${API_URL}/${endpoint}"
+function github_get_api {
+
+	local endpoint="$1"
+	local url="${API_URL}/${endpoint}"
 
 	#Send a get request to the GitHub API with authentication
-	curl -s -u "${USERNAME}:${TOKEN}" "url"
+	curl -s -u "${USERNAME}:${TOKEN}" "$url"
 }
 
 
 #Function to list users with read-access to the repository
-function list_users_with_read_access{
-	local endpoint = "repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
+function list_users_with_read_access {
+
+	local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
 
 	#Fetch the list of collaborators on the repository
-	collaborators = "$(github_get_api "$endpoint" | jq -r '.[] | select(.permission.pull == true) | .login')"	
+	collaborators="$(github_get_api "$endpoint" | jq -r '.[] | select(.permission.pull == true) | .login')"	
 
 	#Display the list of collaborators with read access
-	if [[ -z collaborators  ]]; then
+	if [[ -z "$collaborators"  ]]; then
 		echo "No users with the read access fourn for ${REPO_OWNER}/${$REPO_NAME}."
 	else
 		echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}."
@@ -48,4 +50,4 @@ function list_users_with_read_access{
 
 #Main Script
 echo "Listing the users with Read Access to ${REPO_OWNER}/${REPO_NAME}..."
-list_users_with_read_acess
+list_users_with_read_access
